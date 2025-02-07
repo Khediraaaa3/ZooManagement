4cee5fb9cf6f63068d20a871aa8864ab6409847e
Khediraaaa3 <melekkhedira@gmail.com>
2025-02-07 20:56:52 +0100

    ZooManagement

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class ZooManagement {
+    public static void main(String[] args) {
+        Animal lion = new Animal("lion", "Hugo", 8, true);
+        Zoo myZoo = new Zoo("Frigya", "Tunis", 25);
+        myZoo.displayZoo();
+        System.out.println(myZoo);
+        System.out.println(myZoo.toString());
+    }
 }
