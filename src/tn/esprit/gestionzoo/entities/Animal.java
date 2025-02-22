9b0dd927eeaf6099dca8cd349c5386dbfe019c80
Khediraaaa3 <melekkhedira@gmail.com>
2025-02-22 22:47:22 +0100

    first commit

--- src/tn/esprit/gestionzoo/entities/Animal.java
+++ src/tn/esprit/gestionzoo/entities/Animal.java
@@ -5,6 +5,9 @@ public class Animal {
     public int age;
     public boolean isMammal;
 
+    public Animal() {
+    }
+
     public Animal(String family, String name, int age, boolean isMammal) {
         this.family = family;
         this.name = name;
--- src/tn/esprit/gestionzoo/main/ZooManagement.java
+++ src/tn/esprit/gestionzoo/main/ZooManagement.java
@@ -1,32 +1,32 @@
 package tn.esprit.gestionzoo.main;
 
-import tn.esprit.gestionzoo.entities.Animal;
-import tn.esprit.gestionzoo.entities.Zoo;
+import tn.esprit.gestionzoo.entities.*;
 
 public class ZooManagement {
-    public ZooManagement() {
-    }
-
     public static void main(String[] args) {
-        Animal lion = new Animal("lion", "Hugo", 8, true);
-        Zoo myZoo = new Zoo("Frigya", "Tunis", 25);
-        /*myZoo.displayZoo();
-        System.out.println(myZoo);
-        System.out.println(myZoo.toString());*/
 
-        /*for (int i=0; i<30; i++){
-           System.out.println(myZoo.addAnimal(lion));
-        }*/
-
-        System.out.println(myZoo.addAnimal(lion));
-        Animal lion1 = new Animal("lion", "Hugo", 8, true);
-        System.out.println(myZoo.searchAnimal(lion));
-        System.out.println(myZoo.searchAnimal(lion1));
-
-        Animal ganfoud = new Animal("ganfoud", "Sonic", 4, true);
-        System.out.println(myZoo.addAnimal(ganfoud));
+        Aquatic aquatic = new Aquatic();
+        Terrestrial terrestrial = new Terrestrial();
+        Dolphin dolphin = new Dolphin();
+        Penguin penguin = new Penguin();
 
 
+        Aquatic aquatic2 = new Aquatic("Poisson", "Nemo", 2, false, "Océan");
+        Terrestrial terrestrial2 = new Terrestrial("Félin", "Lion", 5, true, 4);
+        Dolphin dolphin2 = new Dolphin("Cétacé", "Flipper", 10, true, "Océan", 40.0f);
+        Penguin penguin2 = new Penguin("Manchot", "Tux", 3, true, "Antarctique", 100.0f);
+
+
+        System.out.println("\nAffichage des animaux :");
+        System.out.println(aquatic2);
+        System.out.println(terrestrial2);
+        System.out.println(dolphin2);
+        System.out.println(penguin2);
+
+
+        System.out.println("\nTest de la méthode swim() :");
+        aquatic2.swim();
+        dolphin2.swim();
+        penguin2.swim();
     }
-
 }
